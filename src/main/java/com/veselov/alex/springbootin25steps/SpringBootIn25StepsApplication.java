f3a93765426c7558eaa9d8ab5b1475d2a5644e5c
Roman Pogorelov <rmk140987@gmail.com>
2019-12-08 20:51:48 +0300

    Initial commit

---
+++ src/main/java/com/veselov/alex/springbootin25steps/SpringBootIn25StepsApplication.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.springbootin25steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn25StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn25StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veselov/alex/springbootin25steps/SpringBootIn25StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.springbootin25steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIn25StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
